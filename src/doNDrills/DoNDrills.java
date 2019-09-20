46f5a5fd6db6e99f944e95e2609d4b97633bc554
spatel <shalu.patel1217@gmail.com>
2019-09-20 12:55:21 -0500

    Finished

---
+++ src/doNDrills/DoNDrills.java
@@ -0,0 +1,23 @@
+package doNDrills;
+
+public class DoNDrills {
+    public static void main(String[] args) {
+//        for(int i = 0; i < 10; i++){
+//            System.out.println(i);
+//        }
+        for (int i = 10; i != 0; i--){
+
+            System.out.println(i);
+        }
+//        int value = 1;
+//
+//        for(int i = 1; i < 10; i++){
+//            System.out.println(value);
+//            value *= 2;
+//        }
+
+
+    }
+
+
+}
